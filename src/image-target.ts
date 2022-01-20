3be2ee0b1e2bb583ea5dbfff9091c73e7c5bb6a6
hecrean <hector@hectorcrean.xyz>
2022-01-20 20:32:38 +0000

    switch break

--- src/image-target.ts
+++ src/image-target.ts
@@ -1,7 +1,6 @@
 import { ImageFoundMsg, ImageLostMsg, ImageUpdatedMsg } from './type';
 import { SceneGraphCtx } from './state';
 import { Vector3, Quaternion } from 'three';
-import { uiApi } from '3d-ui';
 
 // this is used to keep track of our image targets...
 export type TargetName = 'Eva-Thoma-front' | 'Eduard-Rohrbach-front' | 'Raphael-Bieri-front' | 'business_card';
@@ -16,21 +15,21 @@ const log = (name: string, detail: Detail) => {
     console.log(`handling event ${name}: details: ${JSON.stringify(detail)}`);
 };
 
-type Transform = {
-    position: THREE.Vector3;
-    rotation: THREE.Quaternion;
-    scale: THREE.Vector3;
-};
+// type Transform = {
+//     position: THREE.Vector3;
+//     rotation: THREE.Quaternion;
+//     scale: THREE.Vector3;
+// };
 
-const getTargetTransform = (detail: Detail): Transform => {
-    const { x: x1, y: x2, z: x3 } = detail.position;
-    const { x: q1, y: q2, z: q3, w: q4 } = detail.rotation;
-    const r = new Vector3(x1, x2, x3);
-    const q = new Quaternion(q1, q2, q3, q4);
-    const s = new Vector3(detail.scale, detail.scale, detail.scale);
+// const getTargetTransform = (detail: Detail): Transform => {
+//     const { x: x1, y: x2, z: x3 } = detail.position;
+//     const { x: q1, y: q2, z: q3, w: q4 } = detail.rotation;
+//     const r = new Vector3(x1, x2, x3);
+//     const q = new Quaternion(q1, q2, q3, q4);
+//     const s = new Vector3(detail.scale, detail.scale, detail.scale);
 
-    return { position: r, rotation: q, scale: s };
-};
+//     return { position: r, rotation: q, scale: s };
+// };
 
 export const onImageFoundListener = (sceneCtx: SceneGraphCtx): ImageFoundMsg => {
     return {
