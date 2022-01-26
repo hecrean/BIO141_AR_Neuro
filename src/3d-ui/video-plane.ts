4515143990a436de7d67896c50b1799b5d147a99
hectorcrean-random42 <hector.c@random42.com>
2022-01-26 10:51:50 +0000

    poster-texture

--- src/3d-ui/video-plane.ts
+++ src/3d-ui/video-plane.ts
@@ -7,7 +7,7 @@ import { option } from 'fp-ts'
 
 export type VideoPlane = {
     videoEl: HTMLVideoElement;
-    posterTexture: Texture | null;
+    posterTexture: Texture;
     videoTexture: VideoTexture;
     mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
 };
@@ -27,7 +27,7 @@ export const createVideoPlane = (asset: AssetsCtx, videoUrl: string, posterUrl: 
 
     const posterTexture = pipe(
         asset.texture.api.get(asset.texture.cache, posterUrl),
-        option.getOrElseW(() => null),
+        option.getOrElseW(() => new Texture()),
     );
 
     const videoTexture = new VideoTexture(videoEl);
@@ -48,12 +48,12 @@ export const createVideoPlane = (asset: AssetsCtx, videoUrl: string, posterUrl: 
 export const videoPlaneHandlers: VideoPlaneHandlers = {
     play: (surface: VideoPlane) => {
         surface.mesh.material.map = surface.videoTexture;
-        surface.mesh.material.needsUpdate = true;
+        surface.mesh.material.map.needsUpdate = true;
         surface.videoEl.play();
     },
     pause: (surface: VideoPlane) => {
         surface.mesh.material.map = surface.posterTexture;
-        surface.mesh.material.needsUpdate = true;
+        surface.mesh.material.map.needsUpdate = true;
         surface.videoEl.pause();
     },
 };
