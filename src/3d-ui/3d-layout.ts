db70059768ad1c9c7a56b2459e9cbd22071df969
hectorcrean-random42 <hector.c@random42.com>
2022-02-14 17:04:37 +0000

    ui

--- src/3d-ui/3d-layout.ts
+++ src/3d-ui/3d-layout.ts
@@ -85,7 +85,7 @@ export const initLayout = (elements: UIElementHandles): Layout => {
         },
         {
             tag: 'auroraAppPanel',
-            transform: {position: [1400 * PIXEL, -675 * PIXEL, 0], rotation: [0,0,0,0], scale: [1,1,1]},
+            transform: {position: [1400 * PIXEL, -590 * PIXEL, 0], rotation: [0,0,0,0], scale: [1,1,1]},
             children: [
                 {
                     transform: {position: [0, 0,  0], rotation: [0,0,0,0], scale: [1,1,1]},
