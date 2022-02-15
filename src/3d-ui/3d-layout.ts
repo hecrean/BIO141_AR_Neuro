6107c39c9d0650e32e4068336f53963358e72d79
hectorcrean-random42 <hector.c@random42.com>
2022-02-15 14:50:34 +0000

    rescale

--- src/3d-ui/3d-layout.ts
+++ src/3d-ui/3d-layout.ts
@@ -37,7 +37,7 @@ export const initLayout = (elements: UIElementHandles): Layout => {
         },
         {
             tag: 'auroraAppPanel',
-            transform: {position: [1200 * PIXEL, -370 * PIXEL, 0], rotation: [0,0,0,0], scale: [1,1,1]},
+            transform: {position: [1200 * PIXEL, -360 * PIXEL, 0], rotation: [0,0,0,0], scale: [1,1,1]},
             children: [
                 {
                     transform: {position: [0, 0,  0], rotation: [0,0,0,0], scale: [1,1,1]},
